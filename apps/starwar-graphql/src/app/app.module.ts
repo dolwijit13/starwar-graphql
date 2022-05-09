ca7a5b11706ad1a9e94ae20bca3b06fe016b50d1
WatcharitNew <watcharit.new@gmail.com>
2022-05-09 16:22:15 +0700

    set up grphql

--- apps/starwar-graphql/src/app/app.module.ts
+++ apps/starwar-graphql/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { CharactersModule } from '../characters/characters.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(
+        process.cwd(),
+        'apps/starwar-graphql/src/schema.gql'
+      ),
+    }),
+    CharactersModule,
+  ],
 })
 export class AppModule {}
