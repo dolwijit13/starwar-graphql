24e214f99ae20432791e8bc24ca67a0a6a3ef9a9
dolwijit13 <dol51890@gmail.com>
2022-05-11 12:03:10 +0700

    Get deleted file back

---
+++ apps/starwar-graphql/src/people/people.resolver.spec.ts
@@ -0,0 +1,122 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request = require('supertest');
+import { PeopleModule } from './people.module';
+
+const mockPeople = [
+  {
+    "id": "cGVvcGxlOjEz",
+    "name": "Chewbacca",
+    "species": {
+      "name": "Wookie",
+      "homeworld": {
+        "name": "Kashyyyk",
+        "diameter": 12765
+      }
+    }
+  },
+  {
+    "id": "cGVvcGxlOjE0",
+    "name": "Han Solo",
+    "species": null
+  }
+]
+const mockGetAllPeople = jest.fn(async () => mockPeople)
+
+jest.mock('../infrastructure/starWar.adapter', () => {
+  return {
+    StarWarAdapter: jest.fn().mockImplementation(() => {
+      return {getAllPeople: mockGetAllPeople};
+    }),
+  };
+});
+
+describe('[e2e] PeopleResolver', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    mockGetAllPeople.mockClear();
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [PeopleModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  const gql = '/graphql';
+
+  describe('getAllPeople', () => {
+    it('should return id and name', () => {
+
+      return (
+        request(app.getHttpServer())
+          .post(gql)
+          .send({
+            query:
+              `
+              query getCharacters {
+                getAllPeople{
+                  id,
+                  name
+                }
+              }
+              `,
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.getAllPeople.sort()).toEqual(mockPeople.sort().map((person) => {
+              return {id: person.id, name: person.name}
+            }))
+            expect(mockGetAllPeople.mock.calls.length).toBe(1)
+            expect(mockGetAllPeople.mock.calls[0]).toEqual([]);
+          })
+      )
+    })
+  })
+
+  it('should return id, species and homeworld name', () => {
+    return (
+      request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query:
+            `
+            query getCharacters {
+              getAllPeople{
+                id,
+                species {
+                  name,
+                  homeworld {
+                    name
+                  }
+                }
+              }
+            }
+            `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.getAllPeople.sort()).toEqual(mockPeople.sort().map((person) => {
+            return {
+              id: person.id,
+              species: person.species ?
+                {
+                  name: person.species?.name,
+                  homeworld: {
+                    name: person.species?.homeworld?.name
+                  }
+                } : null
+            }
+          }));
+          expect(mockGetAllPeople.mock.calls.length).toBe(1)
+          expect(mockGetAllPeople.mock.calls[0]).toEqual([]);
+        })
+    )
+  })
+});
