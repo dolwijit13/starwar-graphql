1a8500a7bf49dcbb3558068f9ae375da1b129b17
WatcharitNew <watcharit.new@gmail.com>
2022-05-11 10:05:03 +0700

    refactor mock test and check call function

--- apps/starwar-graphql/src/characters/characters.e2e.spec.ts
+++ apps/starwar-graphql/src/characters/characters.e2e.spec.ts
@@ -1,121 +1,123 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { CharactersModule } from './characters.module';
-import { StarwarAdapter } from '@/infrastructure/starwar.adapter';
 import { Character } from './character.entity';
 import { INestApplication } from '@nestjs/common';
 
 const gql = '/graphql';
+const characters: Character[] = [
+  {
+    id: '1',
+    name: 'Sky walker',
+    filmConnection: {
+      films: [
+        {
+          id: '1',
+          title: 'movie 1',
+        },
+        {
+          id: '2',
+          title: 'movie 2',
+        },
+      ],
+    },
+  },
+  {
+    id: '2',
+    name: 'Jedi',
+    filmConnection: {
+      films: [
+        {
+          id: '1',
+          title: 'movie 1',
+        },
+        {
+          id: '3',
+          title: 'movie 3',
+        },
+      ],
+    },
+  },
+];
+
+const mockGetAllCharacters = jest.fn().mockResolvedValue(characters);
+const mockGetCharacterByID = jest.fn().mockResolvedValue(characters[0]);
+
+jest.mock('@/infrastructure/starwar.adapter', () => {
+  return {
+    StarwarAdapter: jest.fn().mockImplementation(() => {
+      return {
+        getAllCharacters: mockGetAllCharacters,
+        getCharacterByID: mockGetCharacterByID,
+      };
+    }),
+  };
+});
 
 describe('e2e Characters', () => {
   let app: INestApplication;
-  let starwarAdapter: StarwarAdapter;
-  let characters: Character[];
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [CharactersModule],
     }).compile();
-    starwarAdapter = moduleRef.get<StarwarAdapter>(StarwarAdapter);
-
-    characters = [
-      {
-        id: '1',
-        name: 'Sky walker',
-        filmConnection: {
-          films: [
-            {
-              id: '1',
-              title: 'movie 1',
-            },
-            {
-              id: '2',
-              title: 'movie 2',
-            },
-          ],
-        },
-      },
-      {
-        id: '2',
-        name: 'Jedi',
-        filmConnection: {
-          films: [
-            {
-              id: '1',
-              title: 'movie 1',
-            },
-            {
-              id: '3',
-              title: 'movie 3',
-            },
-          ],
-        },
-      },
-    ];
-
     app = moduleRef.createNestApplication();
     await app.init();
+    mockGetAllCharacters.mockClear();
+    mockGetCharacterByID.mockClear();
   });
 
   describe('characters', () => {
     it('should return an array of characters', async () => {
-      jest
-        .spyOn(starwarAdapter, 'getAllCharacters')
-        .mockResolvedValue(characters);
-
       request(app.getHttpServer())
         .post(gql)
         .send({
           query: `query {characters {id name filmsConnection { films { id title } }}}`,
         })
         .expect(200)
-        .expect({ data: characters });
+        .expect({ data: characters })
+        .expect(mockGetAllCharacters.mock.calls.length, 1)
+        .expect(mockGetAllCharacters.mock.calls[0], []);
     });
 
     it('should return an array of characters with only selected attributes', async () => {
       const result = characters.map((character) => ({ name: character.name }));
-      jest
-        .spyOn(starwarAdapter, 'getAllCharacters')
-        .mockResolvedValue(characters);
-
       request(app.getHttpServer())
         .post(gql)
         .send({
           query: `query {characters { name }}`,
         })
         .expect(200)
-        .expect({ data: result });
+        .expect({ data: result })
+        .expect(mockGetAllCharacters.mock.calls.length, 1)
+        .expect(mockGetAllCharacters.mock.calls[0], []);
     });
   });
 
   describe('getCharacter', () => {
     it('should return a character', async () => {
-      jest
-        .spyOn(starwarAdapter, 'getCharacterByID')
-        .mockResolvedValue(characters[1]);
-
       request(app.getHttpServer())
         .post(gql)
         .send({
           query: `query {getcharacter(id: '1') {id name filmsConnection { films { id title } }}}`,
         })
         .expect(200)
-        .expect({ data: characters[1] });
+        .expect({ data: characters[0] })
+        .expect(mockGetCharacterByID.mock.calls.length, 1)
+        .expect(mockGetCharacterByID.mock.calls[0], ['1']);
     });
 
     it('should return a character with only selected attributes', async () => {
-      const result = { name: characters[1].name };
-      jest
-        .spyOn(starwarAdapter, 'getCharacterByID')
-        .mockResolvedValue(characters[1]);
-
+      const result = { name: characters[0].name };
       request(app.getHttpServer())
         .post(gql)
         .send({
           query: 'query {characters { name }}',
         })
         .expect(200)
-        .expect({ data: result });
+        .expect({ data: result })
+        .expect(mockGetCharacterByID.mock.calls.length, 1)
+        .expect(mockGetCharacterByID.mock.calls[0], ['1']);
     });
   });
 
