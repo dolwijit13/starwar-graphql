dc3d55182a884a391a20adeb9c2a2109372d1e78
dolwijit13 <dol51890@gmail.com>
2022-05-13 11:17:00 +0700

    Use redis on company loader

--- apps/starwar-graphql/src/company/company.loader.ts
+++ apps/starwar-graphql/src/company/company.loader.ts
@@ -1,10 +1,29 @@
-import * as DataLoader from 'dataloader';
+import DataLoader = require('dataloader');
 
 import { Company } from './company.entity';
 import { CompanyService } from './company.service';
+import redis = require('redis')
+
+const client = redis.createClient({
+  url: process.env.redis_url
+});
+client.connect()
 
 export const createCompaniesLoader = (companiesService: CompanyService) => {
   return new DataLoader<number, Company>(async (ids: number[]) => {
-    return await companiesService.getCompaniesByIds(ids);
+    const dataFromRedis = await client.mGet(ids.map((id: number) => `company_${id}`))
+    const companiesFromRedis = dataFromRedis.map((data) => JSON.parse(data) as Company)
+    if(dataFromRedis.includes(null)) {
+      const records = {}
+      const results = await companiesService.getCompaniesByIds(ids);
+      results.forEach((company: Company) => {
+        records[`company_${company.id}`] = JSON.stringify(company);
+      });
+      client.mSet(records)
+
+      return results;
+    }
+
+    return companiesFromRedis;
   });
 }
